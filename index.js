4654a00a8d62d88a8b8740ec51762d08f86a9907
Megan Comeau <mlcomeau99@gmail.com>
2020-07-27 20:22:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -49,9 +49,8 @@ const [ r, o, y, g, b, , v ] = colors
 const [ , , , , , indg,  ] = colors
 
 // Objects
-const { name, color, song, job, partner } = muppet
 
 // 7. Use destructuring to assign all appropriate variables using the keys as the variable names
-
+const { name, color, song, job, partner } = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
