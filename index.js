f1d20afba7c9fc92d240468d317afb0e0de4ab92
Megan Comeau <mlcomeau99@gmail.com>
2020-07-27 20:23:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -54,3 +54,5 @@ const [ , , , , , indg,  ] = colors
 const { name, color, song, job, partner } = muppet
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+const { song_2, song_4, k_job, k_partner }
+
